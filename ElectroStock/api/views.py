ef26f5aabfc2964bfe82d8555e69dbabd4771b91
Tomas Guell <tomasguell123@gmail.com>
2023-04-18 10:57:59 -0300

    primer commit guell

---
+++ ElectroStock/api/views.py
@@ -0,0 +1,47 @@
+
+ 
+
+from django.http import response
+from django.shortcuts import render
+#from rest_framework.response import Response
+#from rest_framework.decorators import api_view
+#from rest_framework.serializers import Serializer
+from .models import Note
+#from .serializers import NoteSerializer
+from api import serializers
+#from .utils import updateNote, getNoteDetail, deleteNote, getNotesList, 
+
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
+    ]
+    return response(routes)
