6a053ddf096dda82983555e26087b23052ac929b
FranciscoGiayetto <frangiayetto@gmail.com>
2023-04-19 08:30:48 -0300

    ✨✨

--- ElectroStock/ElectroStockApp/models.py
+++ ElectroStock/ElectroStockApp/models.py
@@ -37,3 +37,50 @@ class SubCategoria(models.Model):
 
     class Meta:
         verbose_name_plural = "SubCategorias"
+
+class LugarCompra(models.Model):
+    lugarcompra = models.CharField(max_length=40)
+    descripcion = models.TextField(max_length=100, null=True, blank=True)
+    def __str__(self):
+        return self.lugarcompra
+    class Meta:
+        verbose_name_plural = "Lugares de Compras"
+        verbose_name = "Lugar Compra"
+
+class Especialidad(models.Model):
+    especialidad = models.CharField(max_length=40)
+    def __str__(self):
+        return self.especialidad
+    class Meta:
+        verbose_name_plural = "Especialidades"
+
+class Laboratorio(models.Model):
+    laboratorio = models.CharField(max_length=40)
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.laboratorio
+    class Meta:
+        verbose_name_plural = "Laboratorios"
+
+class Ubicacion(models.Model):
+    ubicacion = models.CharField(max_length=40)
+    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    def __str__(self):
+        return (f"{self.laboratorio},{self.ubicacion}")
+    class Meta:
+        verbose_name_plural = "Laboratorios"
+    
+class Estado(models.Model):
+    estado = models.CharField(max_length=40)   
+    def __str__(self):
+        return self.estado
+    class Meta:
+        verbose_name_plural = "Estados"
+        
+class Inventario(models.Model):
+    ubicacion = models.CharField(max_length=40)
+    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    def __str__(self):
+        return (f"{self.laboratorio},{self.ubicacion}")
+    class Meta:
+        verbose_name_plural = "Laboratorios"
