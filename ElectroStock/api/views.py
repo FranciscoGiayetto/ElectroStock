b42427d2935b9adb2934ed11c566db0277cad5a4
FranciscoGiayetto <frangiayetto@gmail.com>
2023-05-06 12:04:56 -0300

    📚 Documentacion de la API 📚

--- ElectroStock/api/views.py
+++ ElectroStock/api/views.py
@@ -2,27 +2,24 @@ from django.http import response, JsonResponse
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-#from rest_framework.serializers import Serializer
 from ElectroStockApp import models
-#from .serializers import NoteSerializer
 from .serializers import *
 from rest_framework import viewsets, permissions
 from .permissions import PermisoUsuarioActual
 
 class ElementsViewSet(viewsets.ModelViewSet):
     queryset= models.Element.objects.all()
-    #permisos de lo que pueden ver
     permission_classes = [permissions.AllowAny]
     serializer_class= ElementSerializer
 
 class ProductosEcommerceAPIView(viewsets.ModelViewSet):
     queryset = models.Element.objects.filter(ecommerce=True)
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ElementEcommerceSerializer
 
 class ProductosDetalleAPIView(viewsets.ModelViewSet):
     queryset = models.Element.objects.filter(ecommerce=True)
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ElementDetalleSerializer
 
 class PrestamoVerAPIView(viewsets.ModelViewSet):
@@ -32,5 +29,5 @@ class PrestamoVerAPIView(viewsets.ModelViewSet):
 
 class PrestamoAPIView(viewsets.ModelViewSet):
     queryset = models.Loan.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = PrestamoSerializer
