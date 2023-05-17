1f8edd48820306d286e8e1e1e5690e2d6990f5b2
Tomas Guell <tomasguell123@gmail.com>
2023-05-17 08:42:38 -0300

    detalle Producto inicio del fetch

--- frontend/src/pages/DetalleProducto.js
+++ frontend/src/pages/DetalleProducto.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { useParams, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 function DetalleProducto(props) {
-/*
 
-LOGICA
-*/
+
+  const elementId = useParams();
+  const [element, setElement] = useState(null);
+
+  useEffect(() =>{
+    getElement()
+  }, [elementId])
+
+  const getElement = async () => {
+    let response = await fetch()
+
+  }
+
+
+
+
   return (
     /*
 
