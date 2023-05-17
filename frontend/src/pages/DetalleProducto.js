f7169e1d134001e5d39ea0c9929809cdb46f25bd
Tomas Guell <tomasguell123@gmail.com>
2023-05-17 08:26:06 -0300

    inicio detalleProducto

---
+++ frontend/src/pages/DetalleProducto.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function DetalleProducto(props) {
+/*
+
+LOGICA
+*/
+  return (
+    /*
+
+HTML CON FUNCS ASI {}
+*/
+    <div>
+    <h1>este item es {props.name}</h1>
+    </div>
+  )
+}
+
+DetalleProducto.propTypes = {}
+
+export default DetalleProducto
