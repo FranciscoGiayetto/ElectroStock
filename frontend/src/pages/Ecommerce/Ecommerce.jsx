7f36f54c58059c7fa648d31c0c05a16dd46a8fbb
Tomas Guell <tomasguell123@gmail.com>
2023-06-01 08:32:44 -0300

    no funca

--- frontend/src/pages/Ecommerce/Ecommerce.jsx
+++ frontend/src/pages/Ecommerce/Ecommerce.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Ecommerce.css';
+import { CardExample } from '../../components/card/CardExample.jsx';
 function Ecommerce() {
 
 
   return (
     <div>
-        
+    
         
         <h1>HOLA</h1>
-        
+        <CardExample></CardExample>
         </div>
   )
 }
