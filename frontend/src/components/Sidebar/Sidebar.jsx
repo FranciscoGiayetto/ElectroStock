f98c6c4945b85189d8f253722831861a91b36f07
franco <franco@localhost>
2023-06-01 10:38:54 -0300

    AL FIN ANDA!!

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons';
+import { MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import { Button, Breadcrumb, Layout, Menu, theme } from 'antd';
 import AddModeratorRoundedIcon from '@mui/icons-material/AddModeratorRounded';
-import StorageRoundedIcon from '@mui/icons-material/Storage';
+import StorageRoundedIcon from '@mui/icons-material/StorageRounded';
 import PaidRoundedIcon from '@mui/icons-material/PaidRounded';
-
 import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import { UploadOutlined } from '@ant-design/icons';
 
 const { Header, Sider, Content, Header: { Group } } = Layout;
 
@@ -21,78 +21,65 @@ const App = () => {
   };
 
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        style={{
+          position: 'fixed',
+          left: 0,
+          height: '100vh',
+          background: '#2E5266', 
+        }}
+      >
         <div className="demo-logo-vertical" />
-        <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={handleToggleSidebar}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-              color: 'white',
-            }}
-          />
         <Menu
           theme="dark"
           mode="inline"
           defaultSelectedKeys={['1']}
+          style={{ marginTop: 64, background: '#2E5266' }}
           items={[
-            { key: '1', icon: <AddModeratorRoundedIcon />, label: 'Admin' },
-            { key: '2', icon: <StorageRoundedIcon />, label: 'Stock' },
-            { key: '3', icon: <PaidRoundedIcon />, label: 'Presupuesto' },
-            { key: '4', icon: <UploadOutlined />, label: 'Préstamo' },
-            { key: '5', icon: <StoreRoundedIcon />, label: 'Tienda' },
-            { key: '6', icon: <LogoutRoundedIcon />, label: 'Cerrar sesión' },
-          ]}
-        />
+          { key: '0',
+            icon: collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />,
+            label: 'Menú',
+            onClick: handleToggleSidebar,
+            style: { width: 64, height: 64, color: 'white' },
+          },
+          { key: '1', icon: <AddModeratorRoundedIcon />, label: 'Admin' },
+          { key: '2', icon: <StorageRoundedIcon />, label: 'Stock' },
+          { key: '3', icon: <PaidRoundedIcon />, label: 'Presupuesto' },
+          { key: '4', icon: <UploadOutlined />, label: 'Préstamo' },
+          { key: '5', icon: <StoreRoundedIcon />, label: 'Tienda' },
+          { key: '6', icon: <LogoutRoundedIcon />, label: 'Cerrar sesión' },
+  ]}
+/>
+
       </Sider>
-      <Layout>
+      <Layout style={{ marginLeft: collapsed ? 80 : 200 }}>
         <Header
           style={{
             display: 'flex',
             alignItems: 'center',
             padding: 0,
-            background: '#EBEBEB',
+            background: '#2E5266',
+            position: 'fixed',
+            right: 0,
+            left: collapsed ? 80 : 200,
+            zIndex: 1,
           }}
         >
-
-
-
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={['2']}
-            items={new Array(15).fill(null).map((_, index) => {
-              const key = index + 1;
-              return {
-                key,
-                label: `nav ${key}`,
-              };
-            })}
+            
           />
+          
         </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-content">Content</div>
-        </Content>
+        
       </Layout>
     </Layout>
   );
 };
 
 export default App;
-
