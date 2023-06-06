d6abf3b9c345d42e8d827a75e3ae6211f42d7df1
Canepa225 <canepa.igna@gmail.com>
2023-06-06 09:01:33 -0300

    stats working

--- ElectroStock/ElectroStockApp/management/commands/stats.py
+++ ElectroStock/ElectroStockApp/management/commands/stats.py
@@ -6,6 +6,7 @@ class Command(BaseCommand):
     help = 'Carga de datos iniciales en la base de datos'
 
     def handle(self, *args, **options):
+        # Agregar cursos
         grades = [4, 5, 6, 7]
         for grade in grades:
             course, created = Course.objects.get_or_create(grade=grade)
@@ -14,10 +15,24 @@ class Command(BaseCommand):
             else:
                 self.stdout.write(f"Dato ya existente")
 
-        speacialitys = ['electronica','programacion','electromecanica']
-        for speciality in speacialitys:
-            speciality, created = Course.objects.get_or_create(speciality=speciality)
+        # Agregar especialidades
+        specialities = ['electronica', 'programacion', 'electromecanica']
+        for speciality in specialities:
+            speciality_obj, created = Speciality.objects.get_or_create(name=speciality)
             if created:
                 self.stdout.write(self.style.SUCCESS(f"Especialidad {speciality} creada exitosamente."))
+            else:
+                self.stdout.write(f"Dato ya existente")
+
+        # Agregar laboratorios
+        lab_data = [
+            {'name': 'Laboratorio 1', 'speciality': 'electronica'},
+            {'name': 'Laboratorio 2', 'speciality': 'electronica'}
+        ]
+        for data in lab_data:
+            speciality = speciality = Speciality.objects.get(name=data['speciality'])
+            lab, created = Laboratory.objects.get_or_create(name=data['name'], speciality=speciality)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f"Laboratorio '{data['name']}' creado exitosamente."))
             else:
                 self.stdout.write(f"Dato ya existente")
