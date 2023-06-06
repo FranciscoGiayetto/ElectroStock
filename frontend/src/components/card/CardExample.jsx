147bb96e9b4fa1930e17062f0e61c48af9237463
JuanIgnacioGonzalez <juanignaciogonzalez.6c@gmail.com>
2023-06-06 08:04:19 -0300

    mergeando

--- frontend/src/components/card/CardExample.jsx
+++ frontend/src/components/card/CardExample.jsx
@@ -1,10 +1,12 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './CardExample.css';
+import { Link } from 'react-router-dom';
 export default function CardExample(props) {
   const { id, name, title, image, text } = props;
   return (
     <Card className="mx-auto mb-3" style={{ width: '18rem' }}>
+      <Link to={`/detalleProducto/${id}`}>
       <div className="image-container">
       <Card.Img 
         variant="top"
@@ -14,6 +16,9 @@ export default function CardExample(props) {
       />
         <Button variant="primary" className="my-button">+</Button>
       </div>
+    </Link>
+      
+      <hr className="separator" />
       
       <Card.Body>
         <Card.Title>{title}</Card.Title>
