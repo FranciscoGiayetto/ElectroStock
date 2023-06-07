1db818b85612aaec7c1351b6c5a9c02b8e64e28e
tomasguell <tomasguell123@gmail.com>
2023-06-07 19:10:55 -0300

    export arreglado

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,26 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import '../assets/styles/Login.css'
-import { Logo, Login } from '../components/Form.js'
+import '../assets/styles/Login.css';
+import { Logo, Login } from '../components/Form';
 
-export const LoginPage = () => {
+const LoginPage = () => {
   return (
     <div>
       <Container>
-      <Row>
-        <Col xs={12} className="text-xs-center">
-          <div className='top-left-image'><Logo/></div>
-        </Col>
-        <Col className="centered-form">
-        <div className="login-container">
-        <p className="login-heading"><b>Ingresá tus datos para<br/>iniciar sesión</b></p>
-        <div className="login-form">
-          <Login />
-        </div>
-      </div>
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+          <Col xs={12} className="text-xs-center">
+            <div className='top-left-image'><Logo/></div>
+          </Col>
+          <Col className="centered-form">
+            <div className="login-container">
+              <p className="login-heading"><b>Ingresá tus datos para<br/>iniciar sesión</b></p>
+              <div className="login-form">
+                <Login />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
+
+export default LoginPage;
