0edcb9a04bfacfee13d643adf336a49a3c8f8dc4
tomasguell <tomasguell123@gmail.com>
2023-06-07 19:38:57 -0300

    Sidebar de morales arreglada

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -10,6 +10,8 @@ import { UploadOutlined } from '@ant-design/icons';
 import { InputGroup, FormControl, Button } from 'react-bootstrap';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import './Sidebar.css';
+import its from '../../assets/its.png';
+import CachedRoundedIcon from '@mui/icons-material/CachedRounded';
 
 const { Header, Sider } = Layout;
 const App = () => {
@@ -29,78 +31,74 @@ const App = () => {
   return (
     <div>
       <Sider
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
-        style={{
-          position: 'fixed',
-          left: 0,
-          height: '100vh',
-          background: '#2E5266',
-          zIndex : 10,
-        }}
-      >
-        <div className="demo-logo-vertical" />
-
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          style={{ marginTop: 64, background: '#2E5266' }}
-          items={[
-            {
-              key: '0',
-              icon: collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />,
-              onClick: handleToggleSidebar,
-              style: { color: '#EBEBEB' },
-              alignItems: 'center',
-            },
-            { key: '1', icon: <AddModeratorRoundedIcon />, label: 'Admin' },
-            { key: '2', icon: <StorageRoundedIcon />, label: 'Stock' },
-            { key: '3', icon: <PaidRoundedIcon />, label: 'Presupuesto' },
-            { key: '4', icon: <UploadOutlined />, label: 'Préstamo' },
-            { key: '5', icon: <StoreRoundedIcon />, label: 'Tienda' },
-            { key: '6', icon: <LogoutRoundedIcon />, label: 'Cerrar sesión' },
-          ]}
-        />
-      </Sider>
-
-      <Layout style={{ marginLeft: collapsed ? 80 : 200 }}>
-        <Header
+          trigger={null}
+          collapsible
+          collapsed={collapsed}
           style={{
-            display: 'flex',
-            alignItems: 'center',
-            padding: 0,
-            background: '#2E5266',
+            height: '100vh',
+            background: '#EBEBEB',
             position: 'fixed',
-            right: 0,
-            left: collapsed ? 80 : 200,
-            zIndex: 1,
-            
+            left: 0,
+            top: 64,
+            zIndex:10,
+            boxShadow: '2px 0 4px rgba(0, 0, 0, 0.2)', // Aplica sombra a la sidebar
           }}
         >
-          <form onSubmit={handleSearch}>
-            <InputGroup>
-              <FormControl
-                type="text"
-                name="searchBar"
-                placeholder="Buscar productos"
-                style={{
-                  backgroundColor: '#203d4d',
-                  borderColor: '#2E5266',
-                  borderRadius: '20px',
-                }}
-              />
-              <Button
-                variant="primary"
-                type="submit"
-                style={{ backgroundColor: '#2E5266', borderColor: '#2E5266' }}
-              >
-                <SearchRoundedIcon />
-              </Button>
-            </InputGroup>
-          </form>
-        </Header>
+          <Menu
+            theme="light"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            style={{ background: '#EBEBEB' }}
+            items={[
+              {
+                key: '0',
+                icon: collapsed ? <MenuUnfoldOutlined style={{ fontSize: '20px' }} /> : <MenuFoldOutlined style={{ fontSize: '20px' }} />,
+                onClick: handleToggleSidebar,
+                style: { color: 'black' },
+                alignItems: 'center',
+              },
+              <divider />,
+              { key: '1', icon: <StoreRoundedIcon style={{ fontSize: '20px' }} />, label: 'Tienda' },
+              { key: '2', icon: <CachedRoundedIcon style={{ fontSize: '20px' }} />, label: 'Préstamo' },
+              { key: '3', icon: <PaidRoundedIcon style={{ fontSize: '20px' }} />, label: 'Presupuesto' },
+              { key: '4', icon: <StorageRoundedIcon style={{ fontSize: '20px' }} />, label: 'Stock' },
+              { key: '5', icon: <AddModeratorRoundedIcon style={{ fontSize: '20px' }} />, label: 'Admin' },
+              <divider />,
+              { key: '6', icon: <LogoutRoundedIcon style={{ fontSize: '20px' }} />, label: 'Cerrar sesión' },
+            ]}
+          />
+        </Sider>
+
+      <Layout style={{ marginLeft: collapsed ? 80 : 200 }}>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: 0,
+          background: '#2E5266',
+          position: 'fixed',
+          right: 0,
+          left: 0,
+          zIndex: 1,
+          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)', // Aplica sombra a la navbar
+        }}
+      >
+        <img src={its} alt="its" style={{ width: '100px', marginRight: '10px' }} />
+
+        <form onSubmit={handleSearch}>
+          <InputGroup>
+            <FormControl
+              type="text"
+              name="searchBar"
+              placeholder="Buscar productos"
+              style={{ backgroundColor: '#203d4d', borderColor: '#2E5266', borderRadius: '20px' }}
+            />
+            <Button variant="primary" type="submit" style={{ backgroundColor: '#2E5266', borderColor: '#2E5266' }}>
+              <SearchRoundedIcon />
+            </Button>
+          </InputGroup>
+        </form>
+      </Header>
       </Layout>
     </div>
   );
