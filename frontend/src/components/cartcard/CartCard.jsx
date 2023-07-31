07101e0211a726c30aa6cb152d538683d3a972c5
BautistaCimatti <bcimatti2005@gmail.com>
2023-07-31 16:09:35 -0300

    card for shopping cart creation

---
+++ frontend/src/components/cartcard/CartCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CartCard(props) {
+  return (
+    <h1>soy un elemento del carrito</h1>
+  )
+}
--- frontend/src/pages/Carrito/Carrito.jsx
+++ frontend/src/pages/Carrito/Carrito.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Carrito.css'
+import CartCard from '../../components/cartcard/CartCard';
 
 function Carrito() {
   return (
     <div>
       <div className='container container-background'>
-          <h1>a</h1>
+          <h1>{CartCard}</h1>
         </div>
       </div>
   )
