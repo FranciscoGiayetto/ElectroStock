57a40030bc3d65d7ade38e86c31641b13becce35
franco <franco@localhost>
2023-08-03 17:26:16 -0300

    Creacion del componente CardUser

---
+++ frontend/src/components/CardUser/CardUser.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import './CardUser.css'
+
+export default function CardUser() {
+  return(
+  <div>
+    <div className='div'>
+      <Card body className='card'>
+        <p>hola</p>
+      </Card>
+    </div>
+
+  </div> 
+  
+  
+  );
+}
