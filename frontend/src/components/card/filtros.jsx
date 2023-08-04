0c973242bdacc80fe47ed0bb9fe483f8ee189221
Tomas Guell <tomasguell123@gmail.com>
2023-08-04 10:25:00 -0300

    categorias diferenciados

--- frontend/src/components/card/filtros.jsx
+++ frontend/src/components/card/filtros.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import useAxios from '../../utils/useAxios';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 const WordList = () => {
   const api = useAxios();
   const { id } = useParams();
-  const [element, setElement] = useState([]);
-  
+  const [parentCategories, setParentCategories] = useState([]);
+  const [childrenCategories, setChildrenCategories] = useState([]);
+
   useEffect(() => {
     getElement();
   }, []);
 
-  const handleFilter = () => {
-    console.log('funcionan los links!!')    
+  const handleFilter = (categoryId) => {
+    console.log('funcionan los links!!', categoryId);
+    // Implement your logic to handle filtering based on the selected category
   };
 
   const capitalizeFirstLetter = (string) => {
@@ -23,23 +24,43 @@ const WordList = () => {
   const getElement = async () => {
     try {
       const response = await api.get(`/category/`);
-      const data = response.data.map(category => ({
-        ...category,
-        name: capitalizeFirstLetter(category.name)
-      }));
-      setElement(data);
-      console.log(data)
+      const data = response.data;
+      const formattedParentCategories = data.filter((category) => category.category === null);
+      const formattedChildrenCategories = data.filter((category) => category.category !== null);
+
+      setParentCategories(formattedParentCategories);
+      setChildrenCategories(formattedChildrenCategories);
     } catch (error) {
       console.error(error);
     }
   };
-  
+
+  const getCategoryNameById = (categoryId) => {
+    const category = parentCategories.find((item) => item.id === categoryId);
+    return category ? category.name : '';
+  };
+
   return (
-    <div className="word-list" >
-      <h2>Categorias</h2>
+    <div className="word-list">
+      <h2>Categorias Padre</h2>
       <ul>
-        {element.map((item, i) => (
-          <li key={i}><Link onClick={handleFilter}>{item.name}</Link></li>
+        {parentCategories.map((item) => (
+          <li key={item.id}>
+            <div>
+              {item.name}
+            </div>
+            {/* Render children categories as a dropdown */}
+            <select onChange={(e) => handleFilter(parseInt(e.target.value))}>
+              <option value="">Seleccione una categoría hija</option>
+              {childrenCategories
+                .filter((child) => child.category.id === item.id)
+                .map((child) => (
+                  <option key={child.id} value={child.id}>
+                    {item.name} - {child.name}
+                  </option>
+                ))}
+            </select>
+          </li>
         ))}
       </ul>
     </div>
