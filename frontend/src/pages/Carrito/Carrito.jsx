ad7901bf36e2786d4ea3b8319d2410edf52d856c
BautistaCimatti <bcimatti2005@gmail.com>
2023-08-04 08:41:35 -0300

    fix fr

--- frontend/src/pages/Carrito/Carrito.jsx
+++ frontend/src/pages/Carrito/Carrito.jsx
@@ -11,7 +11,7 @@ import {
 
 function Carrito() {
   return (
-    <section className="vh-100 container-bg" style={{ backgroundColor: "white"}}>
+    <section className="container-bg" style={{ backgroundColor: "white"}}>
       <MDBContainer className="h-100">
         <MDBRow className="justify-content-center align-items-center h-100">
           <MDBCol>
@@ -19,11 +19,17 @@ function Carrito() {
               <span className="h2">Shopping Cart </span>
             </p>
 
+            
             <CartCard/>
             <CartCard/>
             <CartCard/>
-        
-        
+            <CartCard/>
+            <CartCard/>
+            <CartCard/>
+            <CartCard/>
+            <CartCard/>
+            <CartCard/>
+            <CartCard/>
 
         <div className="d-flex justify-content-end">
               <MDBBtn color="light" size="lg" className="me-2">
