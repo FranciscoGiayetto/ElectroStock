0bd03d017179717e72f42fbf204e983dea8b1fbf
TomiGenari <tomasgenari@gmail.com>
2023-08-18 08:21:19 -0300

    Pagina prestamos avanzando

--- frontend/src/pages/Prestamos/CardPrestamos.jsx
+++ frontend/src/pages/Prestamos/CardPrestamos.jsx
@@ -1,22 +1,30 @@
-// PrestamosCard.js
+// CardPrestamos.jsx
 
 import React from 'react';
-import './PrestamosCard.css'; 
-import { Link } from 'react-router-dom';
-// PrestamosCard.js
-const PrestamosCard = (props) => {
+import './PrestamosCard.css'; // Importa tu archivo CSS
+import defaultpicture from '../../assets/images/defaultpicture.png';
+
+const PrestamosCard = ({ status, cliente, monto, fecha }) => {
   return (
-    <div className='card'>
-     
-      <h3>status: {props.status}</h3>
-      <p>Cliente: {props.cliente}</p>
-      <p>Monto: {props.monto}</p>
-      <p>Fecha: {props.fecha}</p>
-      {/* Agregar más propiedades de acuerdo a la clase Prestamos */}
+    <div className='prestamo-card'>
+        <div className='img-container'>
+        <img src={defaultpicture} alt='Imagen' className='img-fluid product-details__image' />
+      </div>      
+        <div className='botones-container'>
+        <button className='boton-izquierdo'>Agregar al carrito</button>
+        <button className='boton-izquierdo'>Ver prestamo</button>
+      </div>
+      <div className='contenido-card'>
+        <p>Status: {status}</p>
+        <p>Cliente: {cliente}</p>
+        <p>Monto: {monto}</p>
+        <p>Fecha: {fecha}</p>
+      </div>
     </div>
   );
 };
 
 export default PrestamosCard;
+
 
 
--- frontend/src/pages/Prestamos/Prestamos.jsx
+++ frontend/src/pages/Prestamos/Prestamos.jsx
@@ -1,46 +1,50 @@
 // Prestamos.jsx
 
 import React, { useState, useEffect } from 'react';
-import useAxios from '../../utils/useAxios';
-import PrestamosCard from './CardPrestamos'; // Importar el componente PrestamosCard
+import PrestamosCard from './CardPrestamos';
+import './Prestamos.css'; 
 
 const Prestamos = () => {
   const [prestamos, setPrestamos] = useState([]);
-  let api = useAxios();
 
   useEffect(() => {
-    getPrestamos();
-  }, []);
+    // Simulación de datos de prueba
+    const datosDePrueba = [
+      {
+        status: 'Aprobado',
+        borrower: { username: 'Usuario1' },
+        monto: 1000,
+        fecha: '2023-08-17',
+      },
+      {
+        status: 'Pendiente',
+        borrower: { username: 'Usuario2' },
+        monto: 1500,
+        fecha: '2023-08-18',
+      },
+    ];
 
-  const getPrestamos = async () => {
-    try {
-      const response = await api.get('/prestamos/');
-      let data = await response.data;
-      console.log(data)
-      setPrestamos(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    setPrestamos(datosDePrueba);
+  }, []);
 
   return (
     <div className='container pagecontainer'>
-      <h1>Mis prestamos</h1>
-
-      {prestamos.length > 0 ? (
-        prestamos.map((prestamo) => (
-          
-          <PrestamosCard  
-          
-            status={prestamo.status}
-            cliente={prestamo.borrower.username}
-            monto={prestamo.monto}
-            fecha={prestamo.fecha}
-          />
-        ))
-      ) : (
-        <p>Cargando préstamos...</p>
-      )}
+            <h1 className='titulo'>Mis préstamos</h1>
+      <div className='prestamos-list'>
+        {prestamos.length > 0 ? (
+          prestamos.map((prestamo, index) => (
+            <PrestamosCard
+              key={index}
+              status={prestamo.status}
+              cliente={prestamo.borrower.username}
+              monto={prestamo.monto}
+              fecha={prestamo.fecha}
+            />
+          ))
+        ) : (
+          <p>Cargando préstamos...</p>
+        )}
+      </div>
     </div>
   );
 };
