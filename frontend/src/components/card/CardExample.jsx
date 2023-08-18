d20edd4550bd0e1400e735dc84ef47fed1773de7
JuanIgnacioGonzalez <juanignaciogonzalez.6c@gmail.com>
2023-08-18 11:38:11 -0300

    cambio cards piolas

--- frontend/src/components/card/CardExample.jsx
+++ frontend/src/components/card/CardExample.jsx
@@ -2,28 +2,112 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './CardExample.css';
 import { Link } from 'react-router-dom';
-export default function CardExample(props) {
-  const { id, name, title, image, text } = props;
+import React from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBIcon,
+  MDBRipple,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+
+function CardExample() {
   return (
-    <Card className="mx-auto mb-3" style={{ width: '10rem' }}>
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        
-      </Card.Body> 
-      <Link to={`/detalleProducto/${id}`}>
-      
-        <div className="image-container">
-          <Card.Img 
-            variant="top"
-            src={image}
-            className="imagencss"
-            style={{  maxHeight: '70px auto', width: '50%', display: 'block' }}
-          />
-          <Button variant="primary" className="my-button">+</Button>
-        </div>
-      </Link>    
-       
-    </Card>
+    <MDBContainer fluid>
+      <MDBRow className="justify-content-center mb-3">
+        <MDBCol md="12" xl="10">
+          <MDBCard className="shadow-0 border rounded-3">
+            <MDBCardBody>
+              <MDBRow>
+                <MDBCol md="12" lg="3" className="mb-4 mb-lg-0">
+                  <MDBRipple
+                    rippleColor="light"
+                    rippleTag="div"
+                    className="bg-image rounded hover-zoom hover-overlay"
+                  >
+                    <MDBCardImage
+                      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(4).webp"
+                      fluid
+                      className="w-100"
+                    />
+                    <a href="#!">
+                      <div
+                        className="mask"
+                        style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+                      ></div>
+                    </a>
+                  </MDBRipple>
+                </MDBCol>
+                <MDBCol md="6">
+                  <h5>Quant olap shirts</h5>
+                  <div className="d-flex flex-row">
+                    <div className="text-danger mb-1 me-2">
+                      <MDBIcon fas icon="star" />
+                      <MDBIcon fas icon="star" />
+                      <MDBIcon fas icon="star" />
+                      <MDBIcon fas icon="star" />
+                    </div>
+                    <span>289</span>
+                  </div>
+                  <div className="mt-1 mb-0 text-muted small">
+                    <span>100% cotton</span>
+                    <span className="text-primary"> • </span>
+                    <span>Light weight</span>
+                    <span classNAme="text-primary"> • </span>
+                    <span>
+                      Best finish
+                      <br />
+                    </span>
+                  </div>
+                  <div className="mb-2 text-muted small">
+                    <span>Unique design</span>
+                    <span className="text-primary"> • </span>
+                    <span>For men</span>
+                    <span className="text-primary"> • </span>
+                    <span>
+                      Casual
+                      <br />
+                    </span>
+                  </div>
+                  <p className="text-truncate mb-4 mb-md-0">
+                    There are many variations of passages of Lorem Ipsum
+                    available, but the majority have suffered alteration in some
+                    form, by injected humour, or randomised words which don't
+                    look even slightly believable.
+                  </p>
+                </MDBCol>
+                <MDBCol
+                  md="6"
+                  lg="3"
+                  className="border-sm-start-none border-start"
+                >
+                  <div className="d-flex flex-row align-items-center mb-1">
+                    <h4 className="mb-1 me-1">$14.99</h4>
+                    <span className="text-danger">
+                      <s>$21.99</s>
+                    </span>
+                  </div>
+                  <h6 className="text-success">Free shipping</h6>
+                  <div className="d-flex flex-column mt-4">
+                    <MDBBtn color="primary" size="sm">
+                      Details
+                    </MDBBtn>
+                    <MDBBtn outline color="primary" size="sm" className="mt-2">
+                      Add to wish list
+                    </MDBBtn>
+                  </div>
+                </MDBCol>
+              </MDBRow>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
- 
+
+export default CardExample;
