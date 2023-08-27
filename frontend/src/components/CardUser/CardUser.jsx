7686b26d25deaed85866ada92910f90f5db3d82c
FrancoMDe <franco51moralesdemaria@gmail.com>
2023-08-27 13:41:45 -0300

    Update componente CardUser

--- frontend/src/components/CardUser/CardUser.jsx
+++ frontend/src/components/CardUser/CardUser.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
+
 import './CardUser.css'
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> </link>
+</head>
 
 export default function CardUser() {
-  return(
-  <div>
-    <div className='div'>
-      <Card body className='card'>
-        <p>hola</p>
-      </Card>
-    </div>
-
-  </div> 
-  
-  
+  return (
+    <MDBCard border='none' style={{fontFamily:'Roboto, sans-serif', fontSize:'0.938rem', boxShadow:'0 2px 4px rgba(0, 0, 0, 0.05)'}}>
+      <MDBCardBody>
+        <MDBCardTitle>Card title</MDBCardTitle>
+        <MDBCardText>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
   );
 }
