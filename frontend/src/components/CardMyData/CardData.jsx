0ba83e724884af9d3dcf9e437b12ffcf5b0b089b
FrancoMDe <franco51moralesdemaria@gmail.com>
2023-08-28 18:35:21 -0300

    Update props

--- frontend/src/components/CardMyData/CardData.jsx
+++ frontend/src/components/CardMyData/CardData.jsx
@@ -15,7 +15,7 @@ import {
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> </link>
 </head>
 
-export default function CardMyData() {
+export default function CardMyData(props) {
   return(
     
     <MDBCard className="card-user" alignment='left' border='none' style={{fontFamily: 'Roboto, sans-serif'}}>
@@ -28,13 +28,13 @@ export default function CardMyData() {
         <tbody>
           <tr>
             <th scope='col'>Email:</th>
-            <td scope='col'>franco.moralesdemaria@gmail.com</td>
+            <td scope='col'>{props.email}</td>
             <td scope='col'><EditIcon style={{color:'#2E5266', fontSize:'0.938rem'}} /></td>
           </tr>
           
           <tr>
             <th scope='col'>Usuario:</th>
-            <td scope='col'>Franco Morales Demaria</td>
+            <td scope='col'>{props.username}</td>
             <td scope='col'><EditIcon style={{color:'#2E5266', fontSize:'0.938rem'}} /></td>
           </tr>
 
