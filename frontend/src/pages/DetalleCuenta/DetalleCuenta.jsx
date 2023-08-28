a14ec525a5b1cd898b39a1a8b996f85a0a0bda57
FrancoMDe <franco51moralesdemaria@gmail.com>
2023-08-28 18:05:57 -0300

    Fetch user data

--- frontend/src/pages/DetalleCuenta/DetalleCuenta.jsx
+++ frontend/src/pages/DetalleCuenta/DetalleCuenta.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import CardMyData from '../../components/CardMyData/CardData.jsx';
 import CardUser from '../../components/CardUser/CardUser.jsx';
 import CardPrestamos from '../../components/CardPrestamos/CardPrestamos.jsx';
 import './DetalleCuenta.css'
+import useAxios from '../../utils/useAxios';
 import { useAuthStore } from '../../store/auth';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import axios from 'axios';
 
 
 
@@ -17,18 +19,40 @@ function DetalleCuenta() {
     state.isLoggedIn,
     state.user,
 ]);
-  const userData = user()
+  const [element, setElement] = useState([]);
+  const api = useAxios();
+  const id = 3;
+  const userData = user();
   console.log(userData)
+
+  const getElement = async () => {
+    try {
+      const response = await axios.get(`http://127.0.0.1:8000/api/users/${id}/`);
+      console.log(response);
+      let data = await response.data;
+      setElement(data);
+      console.log(element);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  useEffect(() => {
+    getElement()
+    }
+    ,[]
+  ) ;
+
   return (
-    <Container fluid style={{marginTop:'6.25rem'}}>
+    <Container fluid style={{marginTop:'6rem', marginBottom:'5rem'}}>
       <Row>
         <Col>
-          <CardUser></CardUser>
+          <CardUser name={element.username} course={element.course}></CardUser>
         </Col>
       </Row>
       <Row style={{marginTop:'2rem'}}>
         <Col>
-          <CardMyData></CardMyData>
+          <CardMyData email={element.email}></CardMyData>
         </Col>
       </Row>
       <Row style={{marginTop:'2rem'}}>
