1eb853fb5e3ce4a278749817cbe597604657e131
JuanIgnacioGonzalez <juanignaciogonzalez.6c@gmail.com>
2023-08-27 22:29:18 -0300

    filtros bastante facheros

--- frontend/src/components/card/filtros.jsx
+++ frontend/src/components/card/filtros.jsx
@@ -1,41 +1,82 @@
 import React, { useEffect, useState } from 'react';
 import useAxios from '../../utils/useAxios';
 import { useParams } from 'react-router-dom';
-import { Container, ListGroup } from 'react-bootstrap';
 
 const WordList = () => {
   const api = useAxios();
   const { id } = useParams();
   const [parentCategories, setParentCategories] = useState([]);
+  const [childrenCategories, setChildrenCategories] = useState([]);
 
   useEffect(() => {
     getElement();
   }, []);
+
+  const handleFilter = (categoryId) => {
+    console.log('funcionan los links!!', categoryId);
+    // Implement your logic to handle filtering based on the selected category
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   const getElement = async () => {
     try {
       const response = await api.get(`/category/`);
       const data = response.data;
       const formattedParentCategories = data.filter((category) => category.category === null);
+      const formattedChildrenCategories = data.filter((category) => category.category !== null);
 
       setParentCategories(formattedParentCategories);
+      setChildrenCategories(formattedChildrenCategories);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const getCategoryNameById = (categoryId) => {
+    const category = parentCategories.find((item) => item.id === categoryId);
+    return category ? category.name : '';
+  };
+
   return (
     <div className="word-list">
-      <h2>Categorias</h2>
       <ul>
         {parentCategories.map((item) => (
-          <ListGroup key={item.id}>
+          <li key={item.id}>
             <div>
-              <a href={`/tienda/${item.name}`} className="btn btn-primary">
-                {item.name}
+              <a
+                href={`/tienda/${item.name}`}
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '1.2rem',
+                  color: 'black',             // Añade esta línea para cambiar el color del texto a negro
+                  textDecoration: 'none'      // Añade esta línea para quitar el subrayado del enlace
+                }}
+              >
+                {capitalizeFirstLetter(item.name)}
               </a>
             </div>
-          </ListGroup> 
+            <ul>
+              {childrenCategories
+                .filter((child) => child.category.id === item.id)
+                .map((child) => (
+                  <li key={child.id}>
+                    <a
+                      href={`/tienda/${child.name}`}
+                      style={{
+                        fontSize: '0.9rem',
+                        color: 'black',         // Añade esta línea para cambiar el color del texto a negro
+                        textDecoration: 'none'  // Añade esta línea para quitar el subrayado del enlace
+                      }}
+                    >
+                      {capitalizeFirstLetter(child.name)}
+                    </a>
+                  </li>
+                ))}
+            </ul>
+          </li>
         ))}
       </ul>
     </div>
