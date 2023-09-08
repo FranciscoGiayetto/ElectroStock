6e9925a869a3522813c3e152a2d1da4b4a24dbe8
Canepa225 <canepa.igna@gmail.com>
2023-09-08 11:21:27 -0300

    icono

--- frontend/src/pages/Informes/TasaAprobacion.jsx
+++ frontend/src/pages/Informes/TasaAprobacion.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import useAxios from '../../utils/useAxios';
-import EventBusyIcon from '@mui/icons-material/EventBusy';
+import AddTaskIcon from '@mui/icons-material/AddTask';
 
 const TasaAprobacion = ({ endpoint }) => {
     const api = useAxios();
@@ -18,7 +18,7 @@ const TasaAprobacion = ({ endpoint }) => {
         <div className="container mt-4">
             <ListGroup as="ul" className='wide'>
                 <ListGroup.Item as="li" className='num font-bold'>
-                    <span className='icono-tasa'><EventBusyIcon></EventBusyIcon></span>
+                    <span className='icono-tasa'><AddTaskIcon></AddTaskIcon></span>
                     {approvalRate !== null ? `${approvalRate}%` : 'Cargando...'}
                 </ListGroup.Item>
                 <ListGroup.Item as="li" active>
