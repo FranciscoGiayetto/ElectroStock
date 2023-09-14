2cd6c2720589c81ff929451a8b6ef3e612ece9f2
tomasguell <tomasguell123@gmail.com>
2023-09-14 14:24:31 -0300

    Andan filtros menos Especialidad

--- frontend/src/pages/Presupuestos/DataTable.jsx
+++ frontend/src/pages/Presupuestos/DataTable.jsx
@@ -15,21 +15,55 @@ const DataTable = ({ presupuestos }) => {
 
   const itemsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(0);
+  const [sortColumn, setSortColumn] = useState(null);
+  const [sortDirection, setSortDirection] = useState('asc');
+
+
+  const getRowTextColor = (estado) => {
+    if (estado === "PROGRESO") {
+      return "text-warning"; // Amarillo
+    } else if (estado === "COMPLETADO") {
+      return "text-success"; // Verde
+    }
+    return ""; // El color de texto predeterminado
+  };
 
   const handlePageClick = ({ selected }) => {
     setCurrentPage(selected);
   };
 
   const offset = currentPage * itemsPerPage;
-  const paginatedData = presupuestos.slice(offset, offset + itemsPerPage);
 
-  const getRowBackgroundColor = (estado) => {
-    if (estado === "PROGRESO") {
-      return "text-warning"; // Amarillo
-    } else if (estado === "COMPLETADO") {
-      return "text-success"; // Verde
+  // Función para ordenar los datos en función del estado de clasificación y la columna seleccionada
+  const sortedData = () => {
+    if (!sortColumn) {
+      return presupuestos.slice(offset, offset + itemsPerPage);
     }
-    return "";
+    
+    return [...presupuestos].sort((a, b) => {
+      const valueA = a[sortColumn];
+      const valueB = b[sortColumn];
+
+      if (valueA < valueB) {
+        return sortDirection === 'asc' ? -1 : 1;
+      }
+      if (valueA > valueB) {
+        return sortDirection === 'asc' ? 1 : -1;
+      }
+      return 0;
+    }).slice(offset, offset + itemsPerPage);
+  };
+
+  // Función para cambiar el filtro de clasificación
+  const handleSortChange = (column) => {
+    if (column === sortColumn) {
+      // Si hacemos clic en la misma columna, cambia la dirección de ordenamiento
+      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+    } else {
+      // Si hacemos clic en una nueva columna, establece la columna y la dirección de ordenamiento predeterminada (ascendente)
+      setSortColumn(column);
+      setSortDirection('asc');
+    }
   };
 
   return (
@@ -38,23 +72,31 @@ const DataTable = ({ presupuestos }) => {
       <Table responsive striped bordered hover className="mt-3">
         <thead>
           <tr>
-            <th scope='col'>ID</th>
-            <th scope='col'>Nombre</th>
-            <th scope='col'>Estado</th>
-            <th scope='col'>Especialidad</th>
+            <th scope='col' onClick={() => handleSortChange('id')}>
+              ID {sortColumn === 'id' && (sortDirection === 'asc' ? '▲' : '▼')}
+            </th>
+            <th scope='col' onClick={() => handleSortChange('name')}>
+              Nombre {sortColumn === 'name' && (sortDirection === 'asc' ? '▲' : '▼')}
+            </th>
+            <th scope='col' onClick={() => handleSortChange('status')}>
+              Estado {sortColumn === 'status' && (sortDirection === 'asc' ? '▲' : '▼')}
+            </th>
+            <th scope='col' onClick={() => handleSortChange('speciality.name')}>
+              Especialidad {sortColumn === 'speciality.name' && (sortDirection === 'asc' ? '▲' : '▼')}
+            </th>
           </tr>
         </thead>
         <tbody>
-          {paginatedData.map((presupuesto) => (
+          {sortedData().map((presupuesto) => (
             <tr
               key={presupuesto.id}
               onClick={() => handleRowClick(presupuesto.id)}
-              className={`cursor-pointer ${getRowBackgroundColor(presupuesto.status)}`}
+              className={`cursor-pointer`}
             >
               <td>{presupuesto.id}</td>
               <td>{presupuesto.name}</td>
-              <td className={`cursor-pointer ${getRowBackgroundColor(presupuesto.status)}`}>{presupuesto.status}</td>
-              <td>{presupuesto.speciality.name}</td>
+              <td className={`${getRowTextColor(presupuesto.status)}`}>{presupuesto.status}</td>
+              <td >{presupuesto.speciality.name}</td>
             </tr>
           ))}
         </tbody>
