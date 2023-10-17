6778c8028bac3537ffd234bf7ff51b095062714d
Canepa225 <canepa.igna@gmail.com>
2023-10-17 11:57:21 -0300

    boton regresar a la tienda funcionando

--- frontend/src/pages/DetalleProducto/DetalleProducto.jsx
+++ frontend/src/pages/DetalleProducto/DetalleProducto.jsx
@@ -6,6 +6,7 @@ import { getRefreshToken } from '../../utils/auth';
 import useAxios from '../../utils/useAxios';
 import defaultpicture from '../../assets/images/defaultpicture.png';
 import './DetalleProducto.css';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
 function DetalleProducto() {
   const [user] = useAuthStore((state) => [state.user]);
@@ -13,51 +14,51 @@ function DetalleProducto() {
   const [posRes, setPostRes] = useState('');
   const [element, setElement] = useState(null);
   const [isVerticalLayout, setIsVerticalLayout] = useState(false);
-  const numeroAleatorio = Math.floor(Math.random() * 21) + 5;
-  const [quantity, setQuantity] = useState(1);
-  const navigate = useNavigate();
-  const api = useAxios(); 
-  const { id } = useParams();
+  const numeroAleatorio = Math.floor (Math.random () * 21) + 5;
+  const [quantity, setQuantity] = useState (1);
+  const navigate = useNavigate ();
+  const api = useAxios ();
+  const {id} = useParams ();
 
-  useEffect(() => {
-    console.log(userData);
-    getElement();
-    handleLayoutChange();
-    window.addEventListener('resize', handleLayoutChange);
+  useEffect (() => {
+    console.log (userData);
+    getElement ();
+    handleLayoutChange ();
+    window.addEventListener ('resize', handleLayoutChange);
     return () => {
-      window.removeEventListener('resize', handleLayoutChange);
+      window.removeEventListener ('resize', handleLayoutChange);
     };
   }, [id]);
 
   const getElement = async () => {
     try {
-      const response = await api.get(`/elements/${id}/`);
-      console.log(response);
+      const response = await api.get (`/elements/${id}/`);
+      console.log (response);
       let data = await response.data;
-      setElement(data);
-      console.log(userData.user_id);
+      setElement (data);
+      console.log (userData.user_id);
     } catch (error) {
-      console.error(error);
+      console.error (error);
     }
   };
 
   const handleLayoutChange = () => {
     const isMobileLayout = window.innerWidth < 768;
-    setIsVerticalLayout(!isMobileLayout);
+    setIsVerticalLayout (!isMobileLayout);
   };
 
   const handleIncrement = () => {
-    setQuantity(quantity + 1);
+    setQuantity (quantity + 1);
   };
 
   const handleDecrement = () => {
     if (quantity > 1) {
-      setQuantity(quantity - 1);
+      setQuantity (quantity - 1);
     }
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault ();
 
     let body = {
       box: element.id,
@@ -71,36 +72,53 @@ function DetalleProducto() {
     };
 
     try {
-      const response = await api.post(`/logPost/${userData.user_id}/`, body);
-      console.log(response.data)
-      setPostRes(response.data.response);
-      navigate('/tienda'); 
+      const response = await api.post (`/logPost/${userData.user_id}/`, body);
+      console.log (response.data);
+      setPostRes (response.data.response);
+      navigate ('/tienda');
     } catch (error) {
-      setPostRes(error.response.data);
+      setPostRes (error.response.data);
     }
   };
 
+  const backButtonStyle = {
+    position: 'absolute',
+    top: 20,
+    left: 5, // Ajusta esta coordenada
+  };
+
   return (
-    <div className='container pagecontainer'>
+    <div className="container pagecontainer" style={{position: 'relative'}}>
+      <div style={backButtonStyle}>
+        <Button
+          variant="outline-primary"
+          onClick={() => navigate ('/tienda')}
+        >
+          <ChevronLeftIcon /> 
+        </Button>
+      </div>
       {element && (
         <div className={`row product-details ${isVerticalLayout ? 'vertical-layout' : ''}`}>
-          <div className='col-md-6 product-details__image-container'>
-            <img src={element.image || defaultpicture} alt='Imagen' className='img-fluid product-details__image' />
+          <div className="col-md-6 product-details__image-container">
+            <img
+              src={element.image || defaultpicture}
+              alt="Imagen"
+              className="img-fluid product-details__image"
+            />
           </div>
 
-          <div className='col-md-6 product-details__info-container' style={{ width: '45%' }}>
-            <h1 className='product-details__title'>Nombre: {element.name}</h1>
-            <h1 className='product-details__description'>Descripción: {element.description}</h1>
-            <h1 className='product-details__category'>Categoría: {element.category}</h1>
-            <h1 className='product-details__stock'>Stock: 20</h1>
+          <div className="col-md-6 product-details__info-container" style={{width: '45%'}}>
+            <h1 className="product-details__title">Nombre: {element.name}</h1>
+            <h1 className="product-details__description">Descripción: {element.description}</h1>
+            <h1 className="product-details__category">Categoría: {element.category}</h1>
+            <h1 className="product-details__stock">Stock: 20</h1>
 
-           
             <Button
-              className='botonCarrito'
-              size='lg'
-              style={{ backgroundColor: '#58A4B0', border: '1px solid #58A4B0' }}
-              variant='primary'
-              type='submit'
+              className="botonCarrito"
+              size="lg"
+              style={{backgroundColor: '#58A4B0', border: '1px solid #58A4B0', left: '5px'}} // Ajusta esta coordenada
+              variant="primary"
+              type="submit"
               onClick={handleSubmit}
             >
               Agregar al carrito
