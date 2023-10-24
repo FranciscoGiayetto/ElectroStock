fe61f60ecfa9c016286ba99b81e6bf58f7e2dd41
franco <franco@localhost>
2023-10-24 10:55:24 -0300

    Anda el quantity

--- frontend/src/components/cartcard/CartCard.jsx
+++ frontend/src/components/cartcard/CartCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
 import './CartCard.css';
 import {
@@ -12,7 +12,9 @@ import {
 import Button from 'react-bootstrap/Button';
 
 export default function CartCard(props) {
-  const { id, name, title, image, quantity, handleDelete, handleQuantityChange } = props;
+  const { id, name, title, image, quantity, handleDelete, handleQuantityChange,handleCommentChange, comments } = props;
+
+  const [observation, setObservation] = useState(comments);
 
   const handleInputChange = (e) => {
     const newQuantity = parseInt(e.target.value, 10);
@@ -21,11 +23,17 @@ export default function CartCard(props) {
     handleQuantityChange(id, newQuantity);
   };
 
+  const handleObservationChange = (e) => {
+    const newObservation = e.target.value;
+    console.log(`New comment for item ${id}: ${newObservation}`);
+    
+    handleCommentChange(id,newObservation);
+  };
+
   return (
     <MDBCard className="rounded-3 mb-4">
       <MDBCardBody className="p-4">
         <MDBRow className="align-items-center">
-
           <MDBCol md="2" lg="2" xl="2">
             <MDBCardImage
               className="rounded-3"
@@ -33,21 +41,18 @@ export default function CartCard(props) {
               src={image}
             />
           </MDBCol>
-
           <MDBCol md="3" lg="3" xl="6">
             <p className="lead fw-normal mb-2">{title}</p>
             <MDBInput
-            placeholder='Añadir comentario'
-            className='input-style'>
-            
-            </MDBInput>
-            
+              placeholder='Añadir comentario'
+              value={observation}
+              onChange={handleObservationChange}
+              className='input-style'
+            />
           </MDBCol>
-
           <MDBCol md="3" lg="3" xl="2"
             className="d-flex align-items-center justify-content-around"
           >
-            {/* Use the handleInputChange function to handle quantity changes */}
             <MDBInput
               min={0}
               value={quantity}
@@ -57,11 +62,11 @@ export default function CartCard(props) {
               className='quantity-style'
             />
           </MDBCol>
-
           <MDBCol md="1" lg="1" xl="2" className="d-flex align-items-center justify-content-end">
-            <Button onClick={() => handleDelete(id)} style={{background:'none', border:'none'}}> <ClearRoundedIcon style={{color:'#2E5266'}}/> </Button>{' '}
+            <Button onClick={() => handleDelete(id)} style={{background:'none', border:'none'}}>
+              <ClearRoundedIcon style={{color:'#2E5266'}}/>
+            </Button>
           </MDBCol>
-
         </MDBRow>
       </MDBCardBody>
     </MDBCard>
