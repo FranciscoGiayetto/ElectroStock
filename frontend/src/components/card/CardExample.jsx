44492fabdb3ad3b4108fd9bf9ce715d6d726fcc1
Canepa225 <canepa.igna@gmail.com>
2023-10-24 10:55:23 -0300

    intento de agregaralcarrito cardexample

--- frontend/src/components/card/CardExample.jsx
+++ frontend/src/components/card/CardExample.jsx
@@ -2,9 +2,37 @@ import { Link } from 'react-router-dom';
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useNavigate, useAuthStore } from 'react-router-dom'; // Asegúrate de importar estas dependencias
 
 export default function CardExample(props) {
-  const { id, title, image, current_stock } = props;
+  const { id, title, image, current_stock, element, userData, api, setPostRes } = props;
+  const navigate = useNavigate(); // Agrega useNavigate
+  
+  const handleAddToCart = async (e) => {
+    e.preventDefault();
+
+    let body = {
+      box: element.id,
+      borrower: userData.user_id,
+      lender: userData.user_id,
+      status: 'CAR',
+      quantity: 1,
+      observation: null,
+      dateIn: null,
+      dateOut: null,
+    };
+
+    try {
+      const response = await api.post(`/logPost/${userData.user_id}/`, body);
+      console.log(response.data);
+      setPostRes(response.data.response);
+      navigate('/tienda');
+    } catch (error) {
+      setPostRes(error.response.data);
+    }
+  };
+
+
   return (
     <div className="container-fluid">
       <div className="row justify-content-end mb-3">
@@ -34,20 +62,19 @@ export default function CardExample(props) {
                     <h4 className="mb-1 me-1">{title}</h4>
                     <span className="text-danger"></span>
                   </div>
-                  <h6 className={current_stock ?   "text-success" : "text-danger"}>
-        {current_stock ? "Stock Disponible" : "Stock No Disponible"}
-      </h6>
+                  <h6 className={current_stock ? "text-success" : "text-danger"}>
+                    {current_stock ? "Stock Disponible" : "Stock No Disponible"}
+                  </h6>
                   <div className="d-flex flex-column mt-4">
-                    <Link to={`/detalleProducto/${id}`}>
                       <Button
                         variant="primary"
                         size="sm"
                         className="w-100"
                         style={{ backgroundColor: '#58A4B0', border: 'none' }}
+                        onClick={handleAddToCart} // Agrega el controlador de clic aquí
                       >
                         Agregar al Carrito
                       </Button>
-                    </Link>
                     <Link to={`/detalleProducto/${id}`}>
                       <Button
                         variant="outline-primary"
