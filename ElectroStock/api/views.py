1b0f253bd6e5a2521ec1bbe5f1fb4a56a8643439
FranciscoGiayetto <frangiayetto@gmail.com>
2023-10-25 09:39:02 -0300

    Update serializer for ProductEcommerceAPIView

--- ElectroStock/api/views.py
+++ ElectroStock/api/views.py
@@ -97,43 +97,6 @@ class ProductosEcommerceAPIView(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = ElementEcommerceSerializer2  # Utiliza el serializador correcto
 
-    def list(self, request, *args, **kwargs):
-        elements = super().list(request, *args, **kwargs).data
-
-        for element in elements:
-            element_id = element['id']
-            if element_id is not None:
-                boxes = models.Box.objects.filter(element__id=element_id)
-                box_ids = [box.id for box in boxes]
-
-                total_com = models.Log.objects.filter(
-                    box__id__in=box_ids, status="COM"
-                ).aggregate(total=Sum("quantity"))["total"]
-                total_ped = models.Log.objects.filter(
-                    box__id__in=box_ids, status="PED"
-                ).aggregate(total=Sum("quantity"))["total"]
-                total_rot = models.Log.objects.filter(
-                    box__id__in=box_ids, status="ROT"
-                ).aggregate(total=Sum("quantity"))["total"]
-                total_ap = models.Log.objects.filter(
-                    box__id__in=box_ids, status="AP"
-                ).aggregate(total=Sum("quantity"))["total"]
-
-                if total_com is None:
-                    total_com = 0
-                if total_ped is None:
-                    total_ped = 0
-                if total_ap is None:
-                    total_ap = 0
-                if total_rot is None:
-                    total_rot = 0
-
-                current_stock = total_com - total_ped - total_ap - total_rot
-
-                element['current_stock'] = current_stock
-
-        return Response(elements)
-
 
 @api_view(["GET", "POST"])
 def PrestamoVerAPIView(request, user_id):
