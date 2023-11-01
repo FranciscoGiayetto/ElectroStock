1b00140fd9f19e359a045fdf19892a4b362ea7ef
BautistaCimatti <bcimatti2005@gmail.com>
2023-11-01 10:44:33 -0300

    hover button removal

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -11,7 +11,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CardNotificaciones from './CardNotificaciones';
-import HoverButton from './HoverButton';
 
 function HomePage() {
   const [isLoggedIn, user] = useAuthStore((state) => [
@@ -51,7 +50,7 @@ function HomePage() {
           </div>
         </Col>
       </Row>
-      <HoverButton />
+      
     </Container>
   );
 }
--- frontend/src/pages/Home/HoverButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './HoverButton.css'; // Create a CSS file for styling
-
-function HoverButton() {
-  return <button className="hover-button">Hover Button</button>;
-}
-
-export default HoverButton;
