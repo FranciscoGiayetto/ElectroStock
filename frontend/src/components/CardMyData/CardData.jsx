1d50e7868519421ee552d8e5ad6144faae333bf2
FrancoMDe <franco51moralesdemaria@gmail.com>
2023-11-02 20:22:01 -0300

    botones de edicion

--- frontend/src/components/CardMyData/CardData.jsx
+++ frontend/src/components/CardMyData/CardData.jsx
@@ -1,51 +1,126 @@
-
-import React from 'react';
-
-import './CardData.css'
+import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
-import EditIcon from '@mui/icons-material/Edit';
+import EditRoundedIcon from '@mui/icons-material/Edit';
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import {
   MDBCard,
   MDBCardHeader,
-  MDBCardBody,
 } from 'mdb-react-ui-kit';
-<head>
-  <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> </link>
-</head>
 
 export default function CardMyData(props) {
-  return(
-    
-    <MDBCard className="card-user" alignment='left' border='none' style={{fontFamily: 'Roboto, sans-serif'}}>
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedEmail, setEditedEmail] = useState(props.email);
+  const [editedUsername, setEditedUsername] = useState(props.username);
+  const [editedPassword, setEditedPassword] = useState('**********'); // Por seguridad, no almacenes contraseñas en texto claro.
+
+  const handleEdit = (field) => {
+    if (field === 'email') {
+      setEditedEmail(props.email); // Restablecer el campo editado al valor original al cancelar.
+    } else if (field === 'username') {
+      setEditedUsername(props.username);
+    } else if (field === 'password') {
+      setEditedPassword('**********');
+    }
+    setIsEditing(field);
+  };
+
+  const handleSave = (field) => {
+    if (field === 'email') {
+      // Aquí puedes enviar una solicitud para guardar el nuevo email.
+      console.log('Guardar Email:', editedEmail);
+    } else if (field === 'username') {
+      // Aquí puedes enviar una solicitud para guardar el nuevo usuario.
+      console.log('Guardar Usuario:', editedUsername);
+    } else if (field === 'password') {
+      // Aquí puedes enviar una solicitud para guardar la nueva contraseña.
+      console.log('Guardar Contraseña:', editedPassword);
+    }
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (event, field) => {
+    if (field === 'email') {
+      setEditedEmail(event.target.value);
+    } else if (field === 'username') {
+      setEditedUsername(event.target.value);
+    } else if (field === 'password') {
+      setEditedPassword(event.target.value);
+    }
+  };
+
+  const confirmIcon = <CheckRoundedIcon style={{ color: 'green', cursor: 'pointer', fontSize: '0.938rem' }} />;
+  const cancelIcon = <CloseRoundedIcon style={{ color: 'red', cursor: 'pointer', fontSize: '0.938rem' }}/>;
+
+  return (
+    <MDBCard className="card-user" alignment='left' border='none' style={{ fontFamily: 'Roboto, sans-serif' }}>
       <MDBCardHeader className="card-header">
         Mis Datos
       </MDBCardHeader>
-      
-      <Table hover className="card-table" >
-      
+
+      <Table hover className="card-table">
         <tbody>
           <tr>
             <th scope='col'>Email:</th>
-            <td scope='col'>{props.email}</td>
-            <td scope='col'></td>
+            {isEditing === 'email' ? (
+              <td scope='col'>
+                <input value={editedEmail} onChange={(e) => handleInputChange(e, 'email')} />
+              </td>
+            ) : (
+              <td scope='col'>{props.email}</td>
+            )}
+            <td scope='col'>
+              {isEditing === 'email' ? (
+                <div>
+                  {confirmIcon} {cancelIcon}
+                </div>
+              ) : (
+                <EditRoundedIcon onClick={() => handleEdit('email')} style={{ color: '#2E5266', cursor: 'pointer', fontSize: '0.938rem' }} />
+              )}
+            </td>
           </tr>
-          
+
           <tr>
             <th scope='col'>Usuario:</th>
-            <td scope='col'>{props.username}</td>
-            <td scope='col'></td>
+            {isEditing === 'username' ? (
+              <td scope='col'>
+                <input value={editedUsername} onChange={(e) => handleInputChange(e, 'username')} />
+              </td>
+            ) : (
+              <td scope='col'>{props.username}</td>
+            )}
+            <td scope='col'>
+              {isEditing === 'username' ? (
+                <div>
+                  {confirmIcon} {cancelIcon}
+                </div>
+              ) : (
+                <EditRoundedIcon onClick={() => handleEdit('username')} style={{ color: '#2E5266', cursor: 'pointer', fontSize: '0.938rem' }} />
+              )}
+            </td>
           </tr>
 
           <tr>
             <th scope='col'>Contraseña:</th>
-            <td scope='col'>**********</td>
-            <td scope='col'></td>
+            {isEditing === 'password' ? (
+              <td scope='col'>
+                <input type="password" value={editedPassword} onChange={(e) => handleInputChange(e, 'password')} />
+              </td>
+            ) : (
+              <td scope='col'>**********</td>
+            )}
+            <td scope='col'>
+              {isEditing === 'password' ? (
+                <div>
+                  {confirmIcon} {cancelIcon}
+                </div>
+              ) : (
+                <EditRoundedIcon onClick={() => handleEdit('password')} style={{ color: '#2E5266', cursor: 'pointer', fontSize: '0.938rem' }} />
+              )}
+            </td>
           </tr>
         </tbody>
       </Table>
     </MDBCard>
-  
   );
 }
