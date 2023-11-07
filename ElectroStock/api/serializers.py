773155fc39011b37073cb75c8682696deea5b24b
franco <franco@localhost>
2023-11-07 10:07:35 -0300

    agrego contrasenia a la api

--- ElectroStock/api/serializers.py
+++ ElectroStock/api/serializers.py
@@ -47,25 +47,23 @@ class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Group
         fields = "__all__"
-
+# Para ver y editar todos los datos del especialidad
+class SpecialitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Speciality
+        fields = "__all__"
 
 # Para todos los usuarios
 class UsersSerializer(serializers.ModelSerializer):
     groups = GroupSerializer()
     course = CourseSerializer()
+    specialties= SpecialitySerializer()
+
 
     class Meta:
         model = models.CustomUser
         fields = (
-            "username",
-            "first_name",
-            "last_name",
-            "email",
-            "is_active",
-            "course",
-            "groups",
-            "last_login",
-            "specialties",
+            "__all__"
         )
 
 
@@ -78,11 +76,7 @@ class ElementEcommerceSerializer(serializers.ModelSerializer):
         read_only_fields = ("id", "name", "description", "image", "category","current_stock")
         queryset = models.Element.objects.filter(ecommerce=True)
 
-# Para ver y editar todos los datos del especialidad
-class SpecialitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Speciality
-        fields = "__all__"
+
 
 # Para ver y editar todos los datos del laboratorio
 class LaboratorySerializer(serializers.ModelSerializer):
@@ -307,3 +301,4 @@ class ElementEcommerceSerializer2(serializers.ModelSerializer):
         data["current_stock"] = current_stock
 
         return data
+    
