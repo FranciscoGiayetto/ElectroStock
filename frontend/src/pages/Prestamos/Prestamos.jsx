49bc2aa31f952db021dcef5193dbc224c638a746
Tomas Guell <tomasguell123@gmail.com>
2023-11-09 21:58:37 -0300

    Aceptar rechazar botones

--- frontend/src/pages/Prestamos/Prestamos.jsx
+++ frontend/src/pages/Prestamos/Prestamos.jsx
@@ -5,6 +5,7 @@ import './Prestamos.css';
 import { useAuthStore } from '../../store/auth';
 import PrestamosCardPackage from './PrestamosCardPackage';
 import ModalDetallePrestamo from './ModalDetallePrestamo';
+
 const Prestamos = () => {
   const [user] = useAuthStore((state) => [state.user]);
   const userData = user();
@@ -13,10 +14,33 @@ const Prestamos = () => {
   const [selectedPackage, setSelectedPackage] = useState(null); // State to store selected package
   const [isModalOpen, setIsModalOpen] = useState(false); // State to control modal visibility
   const user_id = userData.user_id;
-  
+
+  const handleApproval = async () => {
+    try {
+      // Realiza una solicitud PUT para aprobar los registros del usuario en el servidor
+      await api.put(`/aprobadoPost/${userData.user_id}/`);
+      // Vuelve a cargar los préstamos actualizados después de la aprobación
+      getPrestamos();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleRejection = async () => {
+    try {
+      // Realiza una solicitud PUT para rechazar los registros del usuario en el servidor
+      await api.put(`/desaprobadoPost/${userData.user_id}/`);
+      // Vuelve a cargar los préstamos actualizados después del rechazo
+      getPrestamos();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     getPrestamos();
   }, []);
+  
 
   const getPrestamos = async () => {
     try {
@@ -31,7 +55,7 @@ const Prestamos = () => {
   };
 
   const openModal = (packageData) => {
-    console.log("Click registrado")
+    console.log("Click registrado");
     setSelectedPackage(packageData);
     setIsModalOpen(true);
   };
@@ -41,43 +65,43 @@ const Prestamos = () => {
     setSelectedPackage(null);
     setIsModalOpen(false);
   };
+
   return (
     <div className="container pagecontainer">
-   <div className="title-container">
-    <h1 className="textito">Mis prestamos</h1>
-  </div>
+      <div className="title-container">
+        <h1 className="textito">Mis préstamos</h1>
+      </div>
 
-   
-           
-  <div>
-      {data.length > 0 ? (
-       
-            data.map((prestamo, index) => (
-              <PrestamosCardPackage
+      <div>
+        {data.length > 0 ? (
+          data.map((prestamo, index) => (
+            <PrestamosCardPackage
               onClick={() => openModal(prestamo)}
-                key={index}
-                image={prestamo.imagen}
-                status={prestamo.estado}
-                name={prestamo.nombre}
-                dateIn={prestamo.dateIn}
-                dateOut={prestamo.dateOut}
-                count={prestamo.count}
-                lista={prestamo.lista}
-                
-              />
-             
-             
-        ))): (
-          
+              key={index}
+              image={prestamo.imagen}
+              status={prestamo.estado}
+              name={prestamo.nombre}
+              dateIn={prestamo.dateIn}
+              dateOut={prestamo.dateOut}
+              count={prestamo.count}
+              lista={prestamo.lista}
+            />
+          ))
+        ) : (
           <p>Cargando préstamos...</p>
-          )}
-       </div>
+        )}
+      </div>
       {isModalOpen && (
-        <ModalDetallePrestamo lista={selectedPackage.lista} onClose={closeModal} />
+        <ModalDetallePrestamo
+          onHandleApproval={handleApproval}
+          onHandleRejection={handleRejection}
+          dateOut={selectedPackage.dateOut}
+          lista={selectedPackage.lista}
+          onClose={closeModal}
+        />
       )}
     </div>
-    );
-  };
-  
+  );
+};
 
 export default Prestamos;
