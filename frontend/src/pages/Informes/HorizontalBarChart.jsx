3c41e984ca4392f0766ef5e2c46a03b4a0918fdc
Canepa225 <canepa.igna@gmail.com>
2023-11-16 11:58:47 -0300

    grafico aparece vacio y color

--- frontend/src/pages/Informes/HorizontalBarChart.jsx
+++ frontend/src/pages/Informes/HorizontalBarChart.jsx
@@ -6,10 +6,15 @@ const HorizontalBarChart = ({ data }) => {
   const chartInstance = useRef(null);
 
   useEffect(() => {
-    if (data.length === 0) return;
+    const defaultData = {
+      labels: ['Label 1', 'Label 2', 'Label 3'], // Coloca etiquetas predeterminadas
+      values: [0, 0, 0], // Coloca valores predeterminados
+    };
 
-    const labels = data.map(item => item.lender__username);
-    const values = data.map(item => item.vencidos_count);
+    const actualData = data || defaultData;
+
+    const labels = actualData.labels;
+    const values = actualData.values;
 
     const ctx = chartRef.current.getContext('2d');
 
@@ -30,19 +35,11 @@ const HorizontalBarChart = ({ data }) => {
               'rgba(255, 99, 132, 0.2)',
               'rgba(255, 159, 64, 0.2)',
               'rgba(255, 205, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(201, 203, 207, 0.2)',
             ],
             borderColor: [
               'rgb(255, 99, 132)',
               'rgb(255, 159, 64)',
               'rgb(255, 205, 86)',
-              'rgb(75, 192, 192)',
-              'rgb(54, 162, 235)',
-              'rgb(153, 102, 255)',
-              'rgb(201, 203, 207)',
             ],
             borderWidth: 1,
           },
