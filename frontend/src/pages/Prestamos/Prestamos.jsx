7e15bf0862b842f9826e8ab83f90729864c87245
JuanIgnacioGonzalez <juanignaciogonzalez.6c@gmail.com>
2023-11-21 09:38:25 -0300

    todo ok

--- frontend/src/pages/Prestamos/Prestamos.jsx
+++ frontend/src/pages/Prestamos/Prestamos.jsx
@@ -12,16 +12,12 @@ import ModalDetallePrestamo from './ModalDetallePrestamo';
 import IconButton from '@mui/material/IconButton';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import Spinner from 'react-bootstrap/Spinner';
 
 <head>
   <link rel="preconnect" href="https://fonts.googleapis.com"></link>
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> </link>
 </head>
-
-
-
 
 const Prestamos = () => {
   const [user] = useAuthStore((state) => [state.user]);
@@ -31,6 +27,8 @@ const Prestamos = () => {
   const [selectedPackage, setSelectedPackage] = useState(null); // State to store selected package
   const [isModalOpen, setIsModalOpen] = useState(false); // State to control modal visibility
   const user_id = userData.user_id;
+  const [showWordListPrestamos, setShowWordListPrestamos] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleApproval = async (dateIn) => {
     try {
@@ -65,7 +63,6 @@ const Prestamos = () => {
 
   const getPrestamos = async () => {
     try {
-      setLoading(true); // Inicia el Spinner
       const response = await api.get(`/prestamosHistorial/${user_id}`);
       console.log(response.data); // Verify the response from the API
       const data = response.data;
@@ -73,8 +70,6 @@ const Prestamos = () => {
       // Extract the dates from the response
     } catch (error) {
       console.error(error);
-    }finally {
-      setLoading(false); // Detiene el Spinner después de la carga
     }
   };
 
@@ -90,16 +85,10 @@ const Prestamos = () => {
     setIsModalOpen(false);
   };
   return (
-    <div className="container pagecontainer">
-   <div className="title-container" >
-    <h1 className="textito">Mis prestamos</h1>
-  </div>
-
-   
-           
-  <div>
-      {data.length > 0 ? (
-       
+    <div className='container pagecontainer' style={{fontFamily: 'Roboto, sans-serif'}}>
+      
+      <div >
+          {data.length > 0 ? (
             data.map((prestamo, index) => (
               <PrestamosCardPackage
                 onClick={() => openModal(prestamo)}
@@ -118,14 +107,15 @@ const Prestamos = () => {
       </div>
 
       {isModalOpen && (
-  <ModalDetallePrestamo
-    onHandleApproval={() => handleApproval(selectedPackage.dateIn)}
-    onHandleRejection={() => handleRejection(selectedPackage.dateIn)}
-    dateOut={selectedPackage.dateOut}
-    lista={selectedPackage.lista}
-    onClose={closeModal}
-  />
-)}
+        <ModalDetallePrestamo
+        onHandleApproval={() => handleApproval(selectedPackage.dateIn)}
+        onHandleRejection={() => handleRejection(selectedPackage.dateIn)}
+        dateOut={selectedPackage.dateOut}
+        lista={selectedPackage.lista}
+        onClose={closeModal}
+        />
+      )}
+
     </div>
   );
 };
